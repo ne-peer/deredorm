ddbca0ba46bc846cb4f9511650aa42a743ff6834
ne-peer <mileage0110@gmail.com>
2017-03-29 18:07:39 +0900

    [add] oauth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OauthComponent } from './oauth/oauth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OauthComponent
   ],
   imports: [
     BrowserModule,
