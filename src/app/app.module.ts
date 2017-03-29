af97bb69ca9803b45ef556f8937177e333852857
ne-peer <mileage0110@gmail.com>
2017-03-29 18:18:58 +0900

    [update] firebase oauth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { OauthComponent } from './oauth/oauth.component';
 
+// firebase seittings
+const firebaseConfig = {
+    apiKey: "AIzaSyBU2kqeQV9OdwOsuu_Cwa_IedVxMkRnk_E",
+    authDomain: "deredorm.firebaseapp.com",
+    databaseURL: "https://deredorm.firebaseio.com",
+    storageBucket: "deredorm.appspot.com",
+    messagingSenderId: "577838942819"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
