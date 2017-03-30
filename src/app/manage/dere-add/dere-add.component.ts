fc797bd1ea6906b2cd41284bc65c1a304e6874b5
ne-peer <mileage0110@gmail.com>
2017-03-30 18:15:50 +0900

    [add] メインコンテンツのスタイル調整、フォームスケルトン作成

--- src/app/manage/dere-add/dere-add.component.ts
+++ src/app/manage/dere-add/dere-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Idol } from '../../models/dere/idol';
+
 @Component({
   selector: 'app-dere-add',
   templateUrl: './dere-add.component.html',
@@ -8,6 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class DereAddComponent implements OnInit {
 
   constructor() { }
+
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+
+  description = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.';
+  model = new Idol(103, 'Kobayakawa Sae', 'cute', this.description);
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 
   ngOnInit() {
   }
