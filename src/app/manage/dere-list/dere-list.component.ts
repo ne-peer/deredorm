8ad91f2fcf74c9c7f853f99e5073ff7c1f7c5339
ne-peer <mileage0110@gmail.com>
2017-03-31 17:08:00 +0900

    [add] dere-list component

---
+++ src/app/manage/dere-list/dere-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dere-list',
+  templateUrl: './dere-list.component.html',
+  styleUrls: ['./dere-list.component.css']
+})
+export class DereListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
