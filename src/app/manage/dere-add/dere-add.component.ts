b5fafd44fbd665f1bfa6b88b11b49bc8a719e4fa
ne-peer <mileage0110@gmail.com>
2017-03-31 16:18:45 +0900

    [update] とりあえず登録できるようになった

--- src/app/manage/dere-add/dere-add.component.ts
+++ src/app/manage/dere-add/dere-add.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Promise } from 'firebase';
+
 import { Idol } from '../../models/dere/idol';
 
 @Component({
@@ -9,24 +12,30 @@ import { Idol } from '../../models/dere/idol';
 })
 export class DereAddComponent implements OnInit {
 
-  constructor() { }
+  idols: FirebaseListObservable<Idol[]>;
 
+  idol = new Idol(1, 'Kobayakawa Sae', 'cute', ''); 
   positions = ['cute', 'cool', 'passion'];
 
-  model = new Idol(103, 'Kobayakawa Sae', 'cute', '');
-
-  submitted = false;
-
-  onSubmit() {
-    this.submitted = true;
+  constructor(private af: AngularFire) {
+    this.idols = this.af.database.list('/idols');
   }
 
-  newIdol() {
-    // regsiter action
+  onSubmit() {
+    // on submit
+  }
+
+  addIdol(newHero: string): Promise<void> {
+    return this.idols.push({
+      id: 2,
+      name: this.idol.name,
+      position: this.idol.position,
+      model: this.idol.model
+    });
   }
 
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  get diagnostic() { return JSON.stringify(this.idol); }
 
   ngOnInit() {
   }
