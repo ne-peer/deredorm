7613bfc776564f0a4e4fe7a04e26b3fbeaa8bc24
ne-peer <mileage0110@gmail.com>
2017-03-31 15:46:02 +0900

    [update] sumbit reactionを実装

--- src/app/manage/dere-add/dere-add.component.ts
+++ src/app/manage/dere-add/dere-add.component.ts
@@ -17,7 +17,13 @@ export class DereAddComponent implements OnInit {
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newIdol() {
+    // regsiter action
+  }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
