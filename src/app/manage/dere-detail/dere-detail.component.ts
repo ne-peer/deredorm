25d73a2901c30c84dd4385c61e0c9e580a7c8c03
ne-peer <mileage0110@gmail.com>
2017-05-01 16:44:43 +0900

    [update] dere-detailを実装

--- src/app/manage/dere-detail/dere-detail.component.ts
+++ src/app/manage/dere-detail/dere-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Promise } from 'firebase';
+
+import { Idol } from '../../models/dere/idol';
+import { Unit } from '../../models/dere/unit';
 
 @Component({
   selector: 'app-dere-detail',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DereDetailComponent implements OnInit {
 
-  constructor() { }
+  // Firebaseと同期したもの
+  afIdols: FirebaseListObservable<Idol[]>;
+  afUnits: FirebaseListObservable<Unit[]>;
 
+  // プロパティ
+  idols: Idol[];
+  units: Unit[];
+  query: string;
+
+  // 表示用
+  idol: Idol;
+
+  /**
+   * コンストラクタ
+   * 
+   * @param af AngularFire
+   */
+  constructor(private activatedRoute: ActivatedRoute, private af: AngularFire) {
+    this.afIdols = this.af.database.list('/core/dere_list');
+    this.afUnits = this.af.database.list('/core/unit_list');
+    
+    // クエリストリング取得
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.query = params['idol'];
+    });
+  }
+
+  /**
+   * 初期表示
+   */
   ngOnInit() {
+    this.afIdols.subscribe(idols => this.idols = idols);
+
+    const afIdol = this.getIdol(this.query);
+    afIdol.subscribe(idol => this.idol = idol);
+
+    this.afUnits.subscribe(unit => this.units = unit);
+  }
+
+  
+  /**
+   * 指定したユニットを取得
+   * 
+   * @param string 
+   */
+  getIdol(key: string): FirebaseObjectObservable<Idol> {
+    return this.af.database.object(`/core/dere_list/${key}`);
   }
 
 }
