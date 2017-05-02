8fc8d8b975c327aed077b5aecfe91a9bdba5ce1d
ne-peer <mileage0110@gmail.com>
2017-05-02 15:40:06 +0900

    [update] ひらがなオートコンプリート実装

--- src/app/action/dere-autocomplete/dere-autocomplete.component.ts
+++ src/app/action/dere-autocomplete/dere-autocomplete.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/startWith';
 
 import { Idol } from '../../models/dere/idol';
@@ -20,30 +21,32 @@ export class DereAutocompleteComponent implements OnInit {
 
   // オートコンプリート用
   dereSearchCtrl: FormControl;
-  filteredIdols: any;
-  suggests = [];
+  filteredIdols: Observable<Idol[]>;
 
   constructor(private af: AngularFire) {
     this.afIdols = this.af.database.list('/core/dere_list');
-    
-    this.dereSearchCtrl = new FormControl();
-    this.filteredIdols = this.dereSearchCtrl.valueChanges
-      .startWith(null)
-      .map(name => this.filterIdols(name));
-  }
-
-  filterIdols(val: string) {
-    return val ? this.suggests.filter(s => new RegExp(`${val}`, 'gi').test(s)) : this.suggests;
   }
 
   ngOnInit() {
-    this.afIdols.subscribe(snapshot => {
-      for (let idol of snapshot) {
-        this.suggests.push(idol.name);
-      }
+    this.idols = [];
 
+    this.afIdols.subscribe(snapshot => {
       this.idols = snapshot;
     });
+
+    this.dereSearchCtrl = new FormControl();
+    this.filteredIdols = this.dereSearchCtrl.valueChanges
+      .startWith(null)
+      .map(idol => idol && typeof idol === 'object' ? idol.name : idol)
+      .map(name => name ? this.filterIdols(name) : this.idols.slice());
+  }
+
+  filterIdols(query: string): Idol[] {
+    return this.idols.filter(idol => new RegExp(`${query}`, 'gi').test(idol.kana));
+  }
+
+  displayFn(idol: Idol): string {
+    return idol ? idol.name : 'ひ';
   }
 
 }
