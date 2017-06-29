b420aa1931e2cd5a80647dc9c671488ca61ffdaf
ne-peer <mileage0110@gmail.com>
2017-06-29 18:28:02 +0900

    [update][wip] entityとか変更

--- src/app/manage/dere-list/dere-list.component.ts
+++ src/app/manage/dere-list/dere-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Promise } from 'firebase';
 
+import { Overview } from '../../models/dere/overview';
 import { Idol } from '../../models/dere/idol';
 import { Unit } from '../../models/dere/unit';
 import { UnitUtilService } from '../../services/manage/unit-util.service';
@@ -15,10 +16,12 @@ import { UnitUtilService } from '../../services/manage/unit-util.service';
 export class DereListComponent implements OnInit {
 
   // Firebaseと同期したもの
+  afOverviews: FirebaseListObservable<Overview[]>;
   afIdols: FirebaseListObservable<Idol[]>;
   afUnits: FirebaseListObservable<Unit[]>;
 
   // プロパティ
+  overviews: Overview[];
   idols: Idol[];
   units: Unit[];
 
@@ -28,6 +31,7 @@ export class DereListComponent implements OnInit {
    * @param db AngularFireDatabase
    */
   constructor(private db: AngularFireDatabase, private unitUtil: UnitUtilService) {
+    this.afOverviews = this.db.list('/core/dere_overview');
     this.afIdols = this.db.list('/core/dere_list');
     this.afUnits = this.db.list('/core/unit_list');
   }
@@ -36,6 +40,7 @@ export class DereListComponent implements OnInit {
    * 初期表示
    */
   ngOnInit() {
+    this.afOverviews.subscribe(overviews => this.overviews = overviews);
     this.afIdols.subscribe(idols => this.idols = idols);
     this.afUnits.subscribe(unit => this.units = unit);
   }
---
+++ src/app/models/dere/overview.ts
@@ -0,0 +1,7 @@
+export class Overview {
+
+    constructor(
+        public id: string
+    ) { }
+
+}
