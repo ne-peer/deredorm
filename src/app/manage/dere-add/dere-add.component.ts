7bb3a100ebe70b24033f384525be46f355e4a984
ne-peer <mileage0110@gmail.com>
2017-07-04 18:16:56 +0900

    [wip] dere-add comment out

--- src/app/manage/dere-add/dere-add.component.ts
+++ src/app/manage/dere-add/dere-add.component.ts
@@ -10,46 +10,46 @@ import { Idol } from '../../models/dere/idol';
   styleUrls: ['./dere-add.component.css']
 })
 export class DereAddComponent implements OnInit {
-
-  // Firebaseと同期したもの
-  idols: FirebaseListObservable<Idol[]>;
-
-  // 画面用オブジェクト
-  idol = new Idol(null, null, null, null, null);
-  types = ['cute', 'cool', 'passion'];
-  
-  // form
-  submitted = false;
-
-  /**
-   * コンストラクタ
-   * 
-   * @param db AngularFireDatabase
-   */
-  constructor(private db: AngularFireDatabase) {
-    this.idols = this.db.list('/core/dere_list');
-  }
-
-  /**
-   * 登録する
-   */
-  addIdol(): Promise<void> {
-    return this.idols.update(
-      this.idol.name,
-      {
-        name: this.idol.name,
-        kana: this.idol.kana,
-        type: this.idol.type,
-        models: this.idol.models.split(','),
-        units: this.idol.units.split(',')
-      });
-  }
-
-  // かいはつよう
-  get diagnostic() { return JSON.stringify(this.idol); }
-
-  ngOnInit() {}
-  
-  onSubmit() { this.submitted = true; }
-
+//
+//  // Firebaseと同期したもの
+//  idols: FirebaseListObservable<Idol[]>;
+//
+//  // 画面用オブジェクト
+//  idol = new Idol(null, null, null, null, null);
+//  types = ['cute', 'cool', 'passion'];
+//  
+//  // form
+//  submitted = false;
+//
+//  /**
+//   * コンストラクタ
+//   * 
+//   * @param db AngularFireDatabase
+//   */
+//  constructor(private db: AngularFireDatabase) {
+//    this.idols = this.db.list('/core/dere_list');
+//  }
+//
+//  /**
+//   * 登録する
+//   */
+//  addIdol(): Promise<void> {
+//    return this.idols.update(
+//      this.idol.name,
+//      {
+//        name: this.idol.name,
+//        kana: this.idol.kana,
+//        type: this.idol.type,
+//        models: this.idol.models.split(','),
+//        units: this.idol.units.split(',')
+//      });
+//  }
+//
+//  // かいはつよう
+//  get diagnostic() { return JSON.stringify(this.idol); }
+//
+ ngOnInit() {}
+//  
+//  onSubmit() { this.submitted = true; }
+//
 }
