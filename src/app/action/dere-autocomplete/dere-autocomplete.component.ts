300473b3969061202e5d521698015846fcff2f4f
ne-peer <mileage0110@gmail.com>
2017-07-04 15:42:33 +0900

    [fix] autocomplete.searchを修正

--- src/app/action/dere-autocomplete/dere-autocomplete.component.ts
+++ src/app/action/dere-autocomplete/dere-autocomplete.component.ts
@@ -58,7 +58,7 @@ export class DereAutocompleteComponent implements OnInit {
       return false;
     }
 
-    this.router.navigate(['/overview', input]);
+    this.router.navigate(['/idol', input]);
     return false;
   }
 
