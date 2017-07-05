6814644f19f1030be7c1c54266127e1742c1a006
ne-peer <mileage0110@gmail.com>
2017-07-05 18:11:46 +0900

    [update] ユニット一覧を実装

--- src/app/manage/unit-list/unit-list.component.ts
+++ src/app/manage/unit-list/unit-list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Promise } from 'firebase';
+
+import { Unit } from '../../models/unit/unit';
+import { UnitUtilService } from '../../services/manage/unit-util.service';
 
 @Component({
   selector: 'app-unit-list',
   templateUrl: './unit-list.component.html',
-  styleUrls: ['./unit-list.component.css']
+  styleUrls: ['./unit-list.component.css'], 
+  providers: [UnitUtilService] 
 })
 export class UnitListComponent implements OnInit {
 
-  constructor() { }
+  // Firebaseと同期したもの
+  afUnits: FirebaseListObservable<Unit[]>;
 
+  // プロパティ
+  units: Unit[];
+
+  /**
+   * コンストラクタ
+   * 
+   * @param db AngularFireDatabase
+   */
+  constructor(private db: AngularFireDatabase, private unitUtil: UnitUtilService) {
+    this.afUnits = this.db.list('/core/unit_list');
+  }
+
+  /**
+   * 初期表示
+   */
   ngOnInit() {
+    this.afUnits.subscribe(units => this.units = units);
   }
 
 }
