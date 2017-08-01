206619c02e197ae38b6256bb98dde2cc1bfa8699
ne-peer <mileage0110@gmail.com>
2017-08-01 13:04:42 +0900

    [fix] コンストラクタのオプション引数の記法を変更

--- src/app/models/api/imasdb/character.ts
+++ src/app/models/api/imasdb/character.ts
@@ -3,23 +3,23 @@ import { Serializable } from '../../base/serializable';
 export class Character extends Serializable {
 
     constructor(
-        public id              = '',
-        public name            = '',
-        public nameRuby        = '',
-        public familyName      = '',
-        public firstName       = '',
-        public familyNameRuby  = '',
-        public firstNameRuby   = '',
-        public isForeignerName = '',
-        public birthMonth      = '',
-        public birthDay        = '',
-        public gender          = '',
-        public isIdol          = '',
-        public characterType   = '',
-        public arrivalDate     = '',
-        public originMedia     = '',
-        public cv              = '',
-        public className       = ''
+        public id?: string,
+        public name?: string,
+        public nameRuby?: string,
+        public familyName?: string,
+        public firstName?: string,
+        public familyNameRuby?: string,
+        public firstNameRuby?: string,
+        public isForeignerName?: string,
+        public birthMonth?: string,
+        public birthDay?: string,
+        public gender?: string,
+        public isIdol?: string,
+        public characterType?: string,
+        public arrivalDate?: string,
+        public originMedia?: string,
+        public cv?: string,
+        public className?: string
     ) {
         // 親コンストラクタの呼び出し
         super();
--- src/app/models/api/imasdb/profile.ts
+++ src/app/models/api/imasdb/profile.ts
@@ -3,21 +3,21 @@ import { Serializable } from '../../base/serializable';
 export class Profile extends Serializable {
 
     constructor(
-        public generation   = '',
-        public age          = '',
-        public height       = '',
-        public weight       = '',
-        public bust         = '',
-        public waist        = '',
-        public hip          = '',
-        public bloodType    = '',
-        public dominantHand = '',
-        public hometown     = '',
-        public hobby        = '',
-        public specialty    = '',
-        public favorite     = '',
-        public memo         = '',
-        public lastUpdate   = ''
+        public generation?: string,
+        public age?: string,
+        public height?: string,
+        public weight?: string,
+        public bust?: string,
+        public waist?: string,
+        public hip?: string,
+        public bloodType?: string,
+        public dominantHand?: string,
+        public hometown?: string,
+        public hobby?: string,
+        public specialty?: string,
+        public favorite?: string,
+        public memo?: string,
+        public lastUpdate?: string
     ) {
         // 親コンストラクタの呼び出し
         super();
