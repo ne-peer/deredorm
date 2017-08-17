af2c5dacb627eef057ec1821793569056a088cb6
ne-peer <mileage0110@gmail.com>
2017-08-17 17:04:12 +0900

    [add] レポート追加画面のSkeletonを作成

--- src/app/components/trend/trend-add-report/trend-add-report.component.ts
+++ src/app/components/trend/trend-add-report/trend-add-report.component.ts
@@ -1,25 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Promise } from 'firebase';
 
 import { ReportTemplateService } from '../../../services/firebase/report-template.service';
+import { Report } from '../../../models/trend/report';
+import { ReportTemplate } from '../../../models/trend/report-template';
+import { ReportTemplateEval } from '../../../models/trend/report-template-eval';
 
 @Component({
   selector: 'app-trend-add-report',
   templateUrl: './trend-add-report.component.html',
   styleUrls: ['./trend-add-report.component.css'],
-  providers: [ ReportTemplateService ]
+  providers: [ReportTemplateService]
 })
 export class TrendAddReportComponent implements OnInit {
 
-  constructor(private afTemplate: ReportTemplateService) {
-    const templateId = '0';
+  afReports: FirebaseListObservable<Report[]>;
+  report = new Report('', '', '', []);
+  templateId: string;
+  submitted = false
+  template: ReportTemplate;
+  evals: ReportTemplateEval[];
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
-    this.afTemplate.fetch(templateId);
+  constructor(private templateRepos: ReportTemplateService, private db: AngularFireDatabase) {
+    // テンプレート読み込み
+    this.templateId = '100';
+    this.templateRepos.fetch(this.templateId);
+    this.template = this.templateRepos.template;
+    this.evals = this.templateRepos.evals;
+
+    // レポートリポジトリの同期
+    this.afReports = this.db.list('/user/report');
   }
 
   ngOnInit() {
   }
 
-  get diagnosticTemp() { return JSON.stringify(this.afTemplate.template); }
-  get diagnosticEval() { return JSON.stringify(this.afTemplate.evals); }
+  add(): Promise<void> {
+    const wip = '0';
+
+    return this.afReports.update(
+      '0',
+      {
+        id: wip,
+        hash: wip,
+        templateId: this.templateId,
+        detail: []
+      });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  doShow() {
+    return JSON.stringify(this.templateRepos.template) !== '{"$value":null}';
+  }
+
+  get diagnostic() { return JSON.stringify(this.report); }
+  get diagnosticTemp() { return JSON.stringify(this.template); }
+  get diagnosticEval() { return JSON.stringify(this.evals); }
 
 }
---
+++ src/app/models/trend/report.ts
@@ -0,0 +1,10 @@
+export class Report {
+
+    constructor(
+        public id: string,
+        public hash: string,
+        public templateId: string,
+        public detail: any[]
+    ) { }
+
+}
