0b7a782daa9533915d951fb5c36539c9ab0207c7
ne-peer <mileage0110@gmail.com>
2017-09-07 18:34:32 +0900

    [update] DB更新に合わせてモデルを変更

--- src/app/models/dere/overview.ts
+++ src/app/models/dere/overview.ts
@@ -3,7 +3,8 @@ export class Overview {
     constructor(
         public id: string,
         public name: string,
-        public kana: string
+        public kana: string,
+        public latest_hash: string
     ) { }
 
 }
