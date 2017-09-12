315bb6958ab57b23cafe3a91861f319ec80d695d
ne-peer <mileage0110@gmail.com>
2017-09-12 18:32:11 +0900

    [update] person_in_units

--- resource/db/maintenance/person_in_unit.js
+++ resource/db/maintenance/person_in_unit.js
@@ -7,23 +7,35 @@ for (let id in json) {
     const unit = json[id];
 
     for (let idx in unit) {
+        const unitid = unit['id'];
         const unitname = unit['name'];
         const members = unit['member'];
 
         for (let ci in members) {
             const person = members[ci];
-            if (person in pkPerson) {
-                if (pkPerson[person].indexOf(unitname) === -1) {
-                    pkPerson[person].push(unitname);
-                }
-            } else {
-                pkPerson[person] = [unitname];
+            if ((person in pkPerson) === false) {
+                // init
+                pkPerson[person] = {units: [], already: []};
+            }
+
+            if (pkPerson[person].already.indexOf(unitname) === -1) {
+                pkPerson[person]['units'].push(
+                    {id: unitid, name: unitname}
+                );
+
+                pkPerson[person]['already'].push(unitname);
             }
         }
     }
 }
 
-const out = JSON.stringify(pkPerson);
+// 出力用に不要なデータを削除
+let result = {};
+for (let person in pkPerson) {
+    result[person] = pkPerson[person]['units'];
+}
+
+const out = JSON.stringify(result);
 
 const fs = require("fs");
 fs.writeFile('person_in_units.json', out);
