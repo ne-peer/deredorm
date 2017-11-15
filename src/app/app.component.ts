f17858384719062fa20f26dd354e40619d7e6f75
ne-peer <mileage0110@gmail.com>
2017-11-15 18:11:10 +0900

    [update] meta keywords, og:url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,9 +30,11 @@ export class AppComponent implements OnInit {
     // add meta information
     this.metaService.addTags([
       { property: 'site_name', content: 'Deredorm - シンデレラガールズ ライブラリ' },
+      { property: 'keywords', content: 'シンデレラガールズ' },
       { property: 'title', content: 'アイドルとユニットの検索' },
       { property: 'description', content: 'Deredormはアイドルマスターシンデレラガールズの情報ライブラリです。キャラクターの所属ユニットやプロフィールを閲覧することができます。' },
       { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'http://deredorm.ne-peer.com' },
       { property: 'og:site_name', content: 'Deredorm - シンデレラガールズ ライブラリ' },
       { property: 'og:title', content: 'アイドルとユニットの検索' },
       { property: 'og:description', content: 'Deredormはアイドルマスターシンデレラガールズの情報ライブラリです。キャラクターの所属ユニットやプロフィールを閲覧することができます。' },
