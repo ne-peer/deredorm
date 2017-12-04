789a3892025473542a00390af4e5dceca123af0a
ne-peer <mileage0110@gmail.com>
2017-12-04 18:25:03 +0900

    [update] starlightdb.cardクライアントを実装

--- src/app/services/api/starlightdb/card.service.ts
+++ src/app/services/api/starlightdb/card.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+
+import { Hosts } from '../../../constants/api/hosts';
+import { Card } from '../../../models/api/starlightdb/card/card';
 
 @Injectable()
 export class CardService {
 
-  constructor() { }
+  public card: Card;
+
+  constructor(private jsonp: Jsonp) { }
+
+  /**
+   * カード取得
+   *
+   * @param string name
+   * @param string incProfile
+   * @return void
+   */
+  public findCharInfo(cardNo: number): void {
+    // request url
+    const requestUrl = Hosts.API_HOST_STARLIGHTDB + '/api/v1/card_t/' + cardNo;
+
+    // connection
+    this.jsonp.get(requestUrl).subscribe(data => {
+      if (data.status !== 200) {
+        console.log(`Web api connection failure. url=[${requestUrl}]`);
+        return '';
+      }
+
+      const res = data.json()['result'][0];
+      const ignoreFields = ['rarity', 'chara', 'skill', 'leadSkill', 'valist'];
+
+      /**
+       * キャラクター情報をシリアライズ
+       */
+      const card = new Card();
+      card.fillFromJSON(res, true, ignoreFields);
+      this.card = card;
+    });
+  }
 
 }
